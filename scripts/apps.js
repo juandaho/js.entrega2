5e02f6937af284bdb9135199222757e946fc1752
Juan David Holguin <juandaho@gmail.com>
2023-02-22 20:43:07 -0500

    Avance 22-feb-2023 - Finalizado

--- scripts/apps.js
+++ scripts/apps.js
@@ -1,93 +1,148 @@
-//Simulador para que le muestra al sicologo, sobre el descuento de un número de citas, donde si el usuario escoge una cita
-//le da un descuento de 1%, si escoge dos citas un descuento de 2% y así hasta llegar a 10 citas, que equivaldrían al 10% de descuento.
-//También el sistema realiza un calculo para corvertir en moneda colombiana, tanto si escoge si el paciente paga en dolares o en euros.
-//Nota: La pagina web se uso usando boostrat de una plantilla (https://startbootstrap.com/theme/freelancer)
+/* Este simulador tiene como finalidad generar un Array de objetos de Pacientes, donde usando funciones de orden superior se realizaran, calculos y busquedas de los pacientes.
 
+Nota: La pagina web se uso usando boostrat de una plantilla (https://startbootstrap.com/theme/freelancer)
+
+ */
 
 //Saludo inicial al usuario
-alert("Bienvenido a nuestro SIMULADOR DE CALCULO. Nota: La información del desarrollo de este ejercicio se encuentra en la consola");
 
-//Variables iniciales
-let nombrePaciente = prompt("Escriba su nombre");
-let numeroConsultas = Number(prompt("Escoja el número de consultas que desea recibir con el especialista - Máximo 10 citas, por cada cita escogida se ofrece un valor de descuento"));
-let tipoMoneda = prompt("Escoja la moneda a pagar donde 1 es Dolar (4.800 COP)  americano y 2 es Euro (5.100 COP)");
-let dolarCOP = 4800;
-let euroCOP = 5100;
-let precioDolares = 30;
-let precioEuro = 25;
-let precioConsulta = 0;
-let validacionCorrecta = false;
+alert(
+  "Bienvenido a nuestro SIMULADOR DE PACIENTES. Nota: La información del desarrollo de este ejercicio se encuentra en la consola"
+);
 
-//Función que realiza el cambio de moneda (Euro a peso colombiano // dolar a peso colombiano)
-function calculoMoneda(valorcConsuta, tasaCambio) {
+//Clase Paciente
 
-    return valorcConsuta * tasaCambio;
-
+class Paciente {
+  constructor(nombre, edad, horas, sicologo) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.horasConsulta = horas;
+    this.sicologo = sicologo;
+  }
 }
 
-//Validación para que el usuario solo escoja entre 1 y 10 números de consultas a tratar con el sicologo, y sino arroja un error
-if (numeroConsultas >= 1 && numeroConsultas <= 10) {
+//Clase Sicologo
 
-    //Validación para que el usuario solo escoja el número 1 (dolar) o número 2 (euro)
-    if (tipoMoneda === "1") {
-        alert("El costo en DOLARES de una consulta es de 30 USD");
-        precioConsulta = calculoMoneda(precioDolares, dolarCOP);
-        validacionCorrecta = true;
-
-
-    } else if (tipoMoneda === "2") {
-
-        alert("El costo en EUROS de una consulta es de 25 Euros");
-        precioConsulta = calculoMoneda(precioEuro, euroCOP);
-        validacionCorrecta = true;
-    }
-
-    else {
-
-        alert("Moneda erronea");
-        validacionCorrecta = false;
-
-    }
-
-}
-else {
-    alert("El número de consultas ingresado es erroneo");
-    validacionCorrecta = false;
+class Sicologo {
+  constructor(nombre, tarifa) {
+    this.nombre = nombre;
+    this.tarifa = tarifa;
+  }
 }
 
-let descuentoCita = 0;
-let porcentajeDescuento = 0;
+//Instancia de dos sicologos con su información respectiva
 
+const sicologo1 = new Sicologo("Diana Perez", 100000);
+const sicologo2 = new Sicologo("Gloria Tamayo", 90000);
 
-// Calculadora de descuento, primero valida que la información anterior, sea verdadera, para realizar los siguientes calculos
-if (validacionCorrecta == true) {
+//Función calcular el costo
+function valorConsulta(costo_sicologo, horas_paciente) {
+  return costo_sicologo * horas_paciente;
+}
 
-    console.log("El precio normal de una consulta es: "+precioConsulta);
-    
-    for (let i = 1; i <= numeroConsultas; i++) {
+// variable de pacientes
+const pacientes = [];
+let nombrePaciente;
+let edadPaciente;
+let horasConsulta;
+let numeroPacientes;
+let sicologoEscogido;
 
-        descuentoCita = 0;
-        porcentajeDescuento = 0;
-        porcentajeDescuento = i / 100;
+//Saludo inicial
+numeroPaciente = Number(
+  prompt("Ingrese el número de pacientes que desea crear en el sistema")
+);
 
-        
-        descuentoCita = (precioConsulta) - (precioConsulta * porcentajeDescuento);
+//Ciclo para que la instancia de Pacientes se llene por medie de un array
+for (let i = 1; i <= numeroPaciente; i++) {
+  nombrePaciente = prompt("Ingrese el nombre del paciente: #" + i);
+  horasPaciente = Number(
+    (edadPaciente = prompt("Ingrese la edad del paciente: " + nombrePaciente))
+  );
+  horasPaciente = Number(
+    prompt("Ingrese las horas que el paciente consultar: " + nombrePaciente)
+  );
 
-        console.log("El valor de: " + nombrePaciente + " (con descuento en pesos Colombianos) "+ porcentajeDescuento*100+" % " +"de la cita: # " + i + " Es de = " + descuentoCita + " Pesos colombianos");
+  // A través de escoger dos números, se escoge 1. Diana Perez 2. Gloria Tamayo
 
+  sicologoEscogido = Number(
+    prompt(
+      "Escoja el espcialista que deseee: Coloca el número 1 => Diana Perez o número 2 => Gloria Tamayo"
+    )
+  );
 
-    }
+  pacientes.push(
+    new Paciente(nombrePaciente, edadPaciente, horasPaciente, sicologoEscogido)
+  );
+}
+
+// Función Fecha para colocar la información de la fecha.
+const hoy = new Date();
+console.log(
+  "La fecha de hoy para presentar nuestra información es: " + hoy.toDateString()
+);
+
+console.log(
+  "A continuación presentamos el valor total de las consulta por cada paciente"
+);
+
+let tarifaSicologo1 = sicologo1.tarifa;
+let tarifaSicologo2 = sicologo2.tarifa;
+
+// For Each que calcula el valor de pacientes por horas consulta.
+
+pacientes.forEach((paciente) => {
+  if (paciente.sicologo === 1) {
+    console.log(
+      "El paciente con nombre: " +
+        paciente.nombre +
+        " Escogio la sicologo Diana Perez y el costo total de las citas es: " +
+        valorConsulta(sicologo1.tarifa, paciente.horasConsulta)
+    );
+  } else if (paciente.sicologo === 2) {
+    console.log(
+      "El paciente con nombre: " +
+        paciente.nombre +
+        " Escogio la sicologo Gloria Tamayo y el costo total de las citas es: " +
+        valorConsulta(sicologo2.tarifa, paciente.horasConsulta)
+    );
+  }
+});
+
+console.log(
+  "A continuación mostramos los pacientes ordenados por orden alfabetico)"
+);
+
+// Ordena los pacientes en orden alfabetico
+
+pacientes.sort((o1, o2) => {
+  if (o1.nombre < o2.nombre) {
+    return -1;
+  } else if (o1.nombre > o2.nombre) {
+    return 1;
+  } else {
+    return 0;
+  }
+});
+
+pacientes.forEach((paciente) => {
+  console.log(paciente.nombre);
+});
+
+// Buscar paciente para ver la sicologa que escogio:
+
+let terminoBuscar = prompt("Ingrese el nombre  paciente a buscar");
+
+const pacienteEncontrado = pacientes.some(
+  (paciente) => paciente.nombre === terminoBuscar
+);
+
+const pacienteBuscado = pacientes.find(
+  (paciente) => paciente.nombre === terminoBuscar
+);
+
+if (pacienteEncontrado === true) {
+  console.log("El paciente SI fue encontrado: " + pacienteBuscado.nombre);
 } else {
-    console.log("Información incorrecta");
-
+  console.log("El paciente NO fue encontrado ");
 }
-
-
-
-console.log("Fin del simulador")
-
-
-
-
-
-
